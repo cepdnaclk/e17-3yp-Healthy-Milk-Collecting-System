bc350194a122f74f250d5c3978626d3b4a70a7cd
Anuruddhacha <e17038@eng.pdn.ac.lk>
2021-07-21 18:10:25 +0530

    commit mobile app ui v1

--- Hybrid Modile App/lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:budget_tracker_ui/pages/root_app.dart';
-import 'package:flutter/material.dart';
-
-void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: RootApp(),
-  ));
-}
