1fde1ed0ba44bbf0bc586bd5263d4190594c9212
Anuruddhacha chandrasekara <74339667+Anuruddhacha@users.noreply.github.com>
2022-11-18 12:20:05 +0530

    Update api.php

--- Laravel Backend/laravel/routes/api.php
+++ Laravel Backend/laravel/routes/api.php
@@ -23,7 +23,7 @@ use App\Http\Controllers\ConnectController;
 //Route::middleware('auth:api')->get('/user', function (Request $request) {
   //  return $request->user();
 //});
-
+//
 
 
 Route::post('/register',[AuthController::class,'register']);
